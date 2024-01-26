00b75dd19968821fb97d2797902161e22211df0a
Gianluca <g1an@ymail.com>
2024-01-26 21:31:53 +0100

    HostPage

--- Assets/Scenes/Scene1Menu.cs
+++ Assets/Scenes/Scene1Menu.cs
@@ -2,26 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
 
-public class Scene1Menu : MonoBehaviour
+public class InputFieldValidator : MonoBehaviour
 {
-    [SerializeField]
-    Button b1;
+    public static string ID;
+    public TMP_InputField inputField;
+    public Button button1;
+    public Button button2;
 
-    [SerializeField]
-    Button b2;
-
-    // Start is called before the first frame update
     void Start()
     {
-        // disable buttons
-        b1.interactable = false;
-        b2.interactable = false;
+        // Disabilita i bottoni
+        button1.interactable = false;
+        button2.interactable = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Controlla se l'input field contiene almeno 4 caratteri
+        if (inputField.text.Length >= 4)
+        {
+            // Abilita i bottoni
+            button1.interactable = true;
+            button2.interactable = true;
+
+           
+        }
+        else
+        {
+            // Disabilita i bottoni
+            button1.interactable = false;
+            button2.interactable = false;
+        }
+    }
+      public void OnButtonClick()
+    {
+        // Genera un ID univoco
+        ID = System.Guid.NewGuid().ToString();
     }
 }
+
